1b2326efc74bc04353e4ded1a74ad7abd1257472
Novita17x <jheffer17x@gmail.com>
2025-10-10 23:03:09 -0500

    V.3 Fertex

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -11,9 +11,8 @@ function Inicio() {
         <div className="hero-wrapper">
           <div className="hero-content">
             <h2 className="hero-subtitle">ARTES MARCIALES</h2>
-            <h1 className="hero-title">FERTEX</h1>
-            <p className="hero-text">TaekwondoFertex - Escuela de artes marciales con 20 años de experiencia.</p>
-            <p className="hero-text">Enseñanza y difusión del Taekwondo ITF, Muay Thai, Capoeira y Preparación física.</p>
+            <h1 className="hero-title typing">FERTEX</h1>
+            <p className="hero-text">TaekwondoFertex - Escuela de artes marciales con 20 años de experiencia. Enseñanza y difusión del Taekwondo ITF, Muay Thai, Capoeira y Preparación Física.</p>
           </div>
         </div>
       </div>
@@ -45,7 +44,6 @@ function Inicio() {
 
         </div>
       </div>
-
     </div>
   );
 }
